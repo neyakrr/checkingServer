b3993fc0d3db6eff7fb19bd661590cb96e978cc6
neyakrr <neyakrr@gmail.com>
2026-02-23 15:04:25 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,27 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class check
+ */
+@WebServlet("/check")
+public class check extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter o = response.getWriter();
+		o.println("welcome to mphasis");
+	}
+
+	
+}
